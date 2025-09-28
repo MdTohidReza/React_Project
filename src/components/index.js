842fabf4c6ed7e1559fb491b336943509db5eb0d
Md Tohid Reza <harishalone786@gmail.com>
2025-09-28 20:38:09 +0545

    Building Pages

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,13 @@ import Container from "./Container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
+import SignUp from "../pages/SignUp";
+import Login from "../pages/Login";
+import AddPost from "../pages/AddPost";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
+import EditPost from "../pages/EditPost";
+import AuthLayout from './AuthLayout';
+import Post from "../pages/Post";
 
-export { Header, Footer,Container, Logo , LogoutBtn,RTE };
+export { Header, Footer,Container, Logo , LogoutBtn,RTE, SignUp, Login, AddPost, PostForm, PostCard, EditPost, Post, AuthLayout };
